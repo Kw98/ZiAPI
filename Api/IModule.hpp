e7ef9b90adadab16275373e71435f4f1dfdf1840
Kw11 <alexandre.caila@epitech.eu>
2019-01-28 11:19:24 +0100

    Api

--- Api/IModule.hpp
+++ Api/IModule.hpp
@@ -7,41 +7,46 @@
 
 #pragma once
 
-#include <type_traits>
 #include <unordered_map>
 #include <string>
 
 namespace Ziapi
 {
-	
-	enum Processing
-	{
-		PREPROCESSING,
-		POSTPROCESSING,
-		BOTH
+	enum hooks_e {
+		hook_pre_parsing,
+		hook_pre_processing,
+		hook_post_processing
 	};
 
-	struct ModuleData
-	{
-		int prePriority;
-		int postPriority;
-		Processing callMoment;
+	struct module_informations_s {
+		hooks_e type;
+		int	priority;
 	};
 	
-	struct Response
-	{
-		int code;
-		std::string reason;
-		std::string version;
-		std::unordered_map<std::string, std::string> headers;
-		std::string msgBody;
+	struct response_s {
+		int								code;
+		std::string							reason;
+		std::string							version;
+		std::unordered_map<std::string, std::string>	headers;
+		std::string							msg_body;
 	};
-	
+
+	struct request_s {
+		std::string							method;
+		std::string							uri;
+		std::string							version;
+		std::unordered_map<std::string, std::string>	headers;
+		std::string							msg_body;
+	};
+
 	class IModule
 	{
 	public:
 		virtual ~IModule() = default;
-		virtual const ModuleData getModuleData() const noexcept = 0;
-		virtual std::conditional<true, std::string, Response> run(const std::conditional<true, std::string, Response> &input) = 0;
+		virtual const module_informations_s getModuleInformations() const noexcept = 0;
+
+		virtual request_s		&run(request_s &) = 0;
+		virtual response_s	&run(response_s &) = 0;
+		virtual std::string	&run(std::string &) = 0;
 	};
 };
--- Api/IZiapi.hpp
+++ Api/IZiapi.hpp
@@ -16,11 +16,13 @@ namespace Ziapi
 	{
 	public:
 		virtual ~IZiapi() = default;
-		virtual std::string runPreprocessingModules(std::string &request) = 0;
-		virtual Response runPostprocessingModules(Response &response) = 0;
+		virtual std::string		&runPreparsingModules(std::string &) = 0;
+		virtual Ziapi::request_s	&runPreprocessingModules(Ziapi::request_s &) = 0;
+		virtual Ziapi::response_s	&runPostprocessingModules(Ziapi::response_s &) = 0;
+
 		virtual bool loadModule(const std::string &name) = 0;
 		virtual bool unloadModule(const std::string &name) = 0;
 	private:
-		virtual void setPriority(IModules module) = 0;
+		virtual void setPriority(Ziapi::IModule module) = 0;
 	};
 };
